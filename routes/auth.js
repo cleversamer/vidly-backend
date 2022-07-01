0c7da1320eca2b046a29afe7ab5e4af004703a77
Samer A <thedev.samer@gmail.com>
2022-07-01 16:42:43 +0300

    added user authentication route

---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const { User } = require("../models/user");
+const { Router } = require("express");
+const router = Router();
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+
+router.post("/", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).send("Invalid email or password.");
+    }
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.status(400).send("Invalid email or password.");
+    }
+
+    res.status(200).send(true);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const customers = require("./customers");
 const movies = require("./movies");
 const rentals = require("./rentals");
 const users = require("./users");
+const auth = require("./auth");
 
 module.exports = (app) => {
   app.use("/api/genres", genres);
@@ -10,4 +11,5 @@ module.exports = (app) => {
   app.use("/api/movies", movies);
   app.use("/api/rentals", rentals);
   app.use("/api/users", users);
+  app.use("/api/auth", auth);
 };
