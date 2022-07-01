ff68dced28ae86dc8e69e72ee330f93268dc4341
Samer A <thedev.samer@gmail.com>
2022-07-01 16:26:20 +0300

    added hash algorithm for passwords & store hashed passwords in mongodb

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,13 @@ const { User } = require("../models/user");
 const { Router } = require("express");
 const router = Router();
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt(10);
+    req.body.password = await bcrypt.hash(req.body.password, salt);
+
     const result = await User.insertMany([
       _.pick(req.body, ["name", "email", "password"]),
     ]);
