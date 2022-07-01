2f847fbcce3b554ac9ad1e00457fdebc03eb2f40
Samer A <thedev.samer@gmail.com>
2022-07-01 14:27:12 +0300

    fixed a bug caused by a typo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const genres = require("./genres");
 const customers = require("./customers");
 const movies = require("./movies");
+const rentals = require("./rentals");
 
 module.exports = (app) => {
   app.use("/api/genres", genres);
   app.use("/api/customers", customers);
   app.use("/api/movies", movies);
+  app.use("/api/rentals", rentals);
 };
