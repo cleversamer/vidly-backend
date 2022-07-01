b76944485427b6b574da82cfbcc71eefc8d5cc03
Samer A <thedev.samer@gmail.com>
2022-07-01 20:09:39 +0300

    added authenticatio middleware

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    return res.status(401).send("Access deined. No token provided.");
+  }
+
+  try {
+    const jwtPayload = jwt.decode(token, process.env["jwtPrivateKey"]);
+    req.user = jwtPayload;
+    next();
+  } catch (err) {
+    res.status(400).send("Invalid token.");
+  }
+};
+
+module.exports = auth;
