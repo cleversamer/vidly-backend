2259c0951edbc46c439ddfd8c9174d1ae7f8541a
Samer A <thedev.samer@gmail.com>
2022-07-02 10:18:39 +0300

    exported services into new independent folder & updated mongoose

--- middleware/error.js
+++ middleware/error.js
@@ -1,21 +1,4 @@
-const winston = require("winston");
-require("winston-mongodb");
-
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
-  transports: [
-    new winston.transports.File({ filename: "errors.log", level: "error" }),
-    new winston.transports.MongoDB({
-      db: process.env["MONGODB_URI"],
-      collection: "logs",
-      level: "error",
-      expireAfterSeconds: 1000 * 60 * 60 * 24, // => 1 day
-      options: { useUnifiedTopology: true },
-    }),
-  ],
-});
+const logger = require("../services/logger");
 
 module.exports = (err, req, res, next) => {
   logger.log({
---
+++ services/logger.js
@@ -0,0 +1,20 @@
+const winston = require("winston");
+require("winston-mongodb");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({ filename: "errors.log", level: "error" }),
+    new winston.transports.MongoDB({
+      db: process.env["MONGODB_URI"],
+      collection: "logs",
+      level: "error",
+      expireAfterSeconds: 1000 * 60 * 60 * 24, // => 1 day
+      options: { useUnifiedTopology: true },
+    }),
+  ],
+});
+
+module.exports = logger;
