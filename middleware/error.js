452f7a3aa02ffa56685de89e113c9b9f6948a02c
Samer A <thedev.samer@gmail.com>
2022-07-02 10:15:09 +0300

    added error logging in a separate file and in mongodb

--- middleware/error.js
+++ middleware/error.js
@@ -1,3 +1,26 @@
+const winston = require("winston");
+require("winston-mongodb");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({ filename: "errors.log", level: "error" }),
+    new winston.transports.MongoDB({
+      db: process.env["MONGODB_URI"],
+      collection: "logs",
+      level: "error",
+      expireAfterSeconds: 1000 * 60 * 60 * 24, // => 1 day
+      options: { useUnifiedTopology: true },
+    }),
+  ],
+});
+
 module.exports = (err, req, res, next) => {
+  logger.log({
+    level: "error",
+    message: err.message,
+  });
   res.status(500).send("Something went wrong on the server.");
 };
