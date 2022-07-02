41a937f720d341559e4eea81bb45c17edb66136c
Samer A <thedev.samer@gmail.com>
2022-07-02 10:39:33 +0300

    diabled the feature of loggin in a local log file

--- services/logger.js
+++ services/logger.js
@@ -6,7 +6,7 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   defaultMeta: { service: "user-service" },
   transports: [
-    new winston.transports.File({ filename: "errors.log", level: "error" }),
+    // new winston.transports.File({ filename: "errors.log", level: "error" }),
     new winston.transports.MongoDB({
       db: process.env["MONGODB_URI"],
       collection: "logs",
